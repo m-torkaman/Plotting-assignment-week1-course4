cc1375dad721dbd452ef340183f2d95362dbaef0
m-torkaman <35469758+m-torkaman@users.noreply.github.com>
2018-02-17 09:25:44 +0000

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,8 @@
+## setting start and end date, as specified in the assignment 
+sdate<-as.Date("2007-02-01")
+edate <- as.Date("2007-02-02")
+
+##reading data and chosing right raws for 2 days, using pipeline
 mydata <- read.table("./pconsumption/household_power_consumption.txt", 
                      stringsAsFactors = FALSE, sep=";", header = TRUE , na.strings = "?" ,
                      colClasses = c("character" ,"character", "numeric", "numeric","numeric","numeric",
@@ -5,7 +10,6 @@ mydata <- read.table("./pconsumption/household_power_consumption.txt",
         filter(dmy(Date) == sdate | dmy(Date)== edate)
 
 ## creating a new column of date and time
-
 mydata$date_time <- dmy_hms(paste(mydata2$Date, mydata2$Time))
 
 
@@ -17,7 +21,6 @@ with(mydata, plot(date_time, Global_active_power ,type="l",
                   xlab="", ylab="Global Active Power",  mar=c(4,2,0,1) ))
 
 ## Second plot (Down Left)
-
 with(mydata, {plot(date_time, Sub_metering_1, type="l",  mar=c(2,2,0,0),
                    ylab= "Energy sub mettering", xlab = "")
         points(x=date_time, y=Sub_metering_2, col="red", type = "l")
